e3e3819123a1ac074ceb6a7319e6cbc64113ead2
Bel <cibeleparra@rockemail.com>
2024-02-28 21:30:53 -0300

    fix: interfaces, package and .env

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,5 @@
 import { CategoryCreate, CategoryReturn } from "./category.interface";
 import { TaskCreate, TaskUpdate, TaskReturn } from "./task.interface";
-import { UserCreate, UserLogin, UserReturn, UserAuth, UserAuth } from "./user.interface";
+import { UserCreate, UserLogin, UserReturn, UserAuth } from "./user.interface";
 
-export { CategoryCreate, CategoryReturn, TaskCreate, TaskUpdate, TaskReturn, UserCreate, UserLogin, UserReturn, UserAuth, UserAuth };
+export { CategoryCreate, CategoryReturn, TaskCreate, TaskUpdate, TaskReturn, UserCreate, UserLogin, UserReturn, UserAuth };
